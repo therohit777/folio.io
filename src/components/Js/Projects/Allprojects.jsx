1c3d7e3dbdd8d3601ad9acea01bb98d4171249a8
Rohit Pandey <rohitpandeyofficial777@gmail.com>
2024-02-08 20:47:35 +0530

    changes added to UI of Journey Sectionm

--- src/components/Js/Projects/Allprojects.jsx
+++ src/components/Js/Projects/Allprojects.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "../../Css/Allprojects.css";
+import { Link } from 'react-router-dom';
 
 const Allprojects = () => {
   return (
-    <div>Allprojects</div>
+    <div className='Allprojects'>
+      <div className="journeyheader"><p className='headjourney'>My <span className='headjtn'>Portfolio</span></p><span className='journeybgntxt'>Portfolio</span></div>
+      <Link to='/home' className='backhome'>Back to home</Link>
+    </div>
   )
 }
 
-export default Allprojects
+export default Allprojects;
